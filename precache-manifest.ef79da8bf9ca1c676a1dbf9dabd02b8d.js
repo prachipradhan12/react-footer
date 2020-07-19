d109bea738a43f93968850bff46884422f4f5bc2
prachipradhan12 <pradhan.prachi10@gmail.com>
2020-07-19 14:29:54 +0530

    Updates

---
+++ precache-manifest.ef79da8bf9ca1c676a1dbf9dabd02b8d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdd118cd6fb493717a0278e29d99b34f",
+    "url": "/react-footer/index.html"
+  },
+  {
+    "revision": "8c52f35f5a9d6a28ed21",
+    "url": "/react-footer/static/css/main.148c1796.chunk.css"
+  },
+  {
+    "revision": "f6be0233b2cdb052aece",
+    "url": "/react-footer/static/js/2.174310db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-footer/static/js/2.174310db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c52f35f5a9d6a28ed21",
+    "url": "/react-footer/static/js/main.52b370db.chunk.js"
+  },
+  {
+    "revision": "9b51285cad5ccb601711",
+    "url": "/react-footer/static/js/runtime-main.588a55ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-footer/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "adba9865e1f3fa407f4ae95e9b0fb155",
+    "url": "/react-footer/static/media/three-wooden-signage-in-a-tree-close-up-photography-2562560.adba9865.jpg"
+  }
+]);
